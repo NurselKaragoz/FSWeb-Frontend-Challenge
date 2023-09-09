b3ee2319e15e23b4b003bdc101dbf70a8e951a6d
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-09-09 17:05:54 +0300

    navigation complete

---
+++ web-sayfam/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NavigatorBar from "./navbar/nav";
+
+function App() {
+  return (
+    <div className="App">
+      <NavigatorBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ web-sayfam/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+
+    colors: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        purple: "#EEEBFF",
+        midnight: "#121063",
+        metal: "#565584",
+        tahiti: "#3ab7bf",
+        silver: "#ecebff",
+        bermuda: "#78dcca",
+        bluex: "#3730A3",
+        graytext: "#6B7280",
+        purpletext: "#7B61FF",
+      },
+    },
+  },
+  plugins: [],
+};
