0d1caa6f4174ca27c0fb97b641b774bfb4bd016b
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-09-15 15:08:24 +0300

    header eklendi

--- web-sayfam/src/Companents/header.js
+++ web-sayfam/src/Companents/header.js
@@ -1,4 +1,11 @@
 function Header() {
-  return <div></div>;
+  return (
+    <div class="flex justify-end">
+      <p class="pr-5  text-colors-graytext">DARK MODE</p>
+      <p class="border-l-2 pl-5 text-colors-graytext">
+        <span class="text-colors-bluex">TÜRKÇE </span>'YE GEÇ
+      </p>
+    </div>
+  );
 }
 export default Header;
