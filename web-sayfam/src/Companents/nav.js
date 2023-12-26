201be878b23bab7f72188f94f3925ffe854a0738
Nursel Karagoz <nurselkaragoz@outlook.com>
2023-12-26 15:07:19 +0300

    vercel check

--- web-sayfam/src/Companents/nav.js
+++ web-sayfam/src/Companents/nav.js
@@ -3,7 +3,7 @@ function NavigatorBar() {
     <div>
       <div className="flex place-content-between dark:bg-colors-black200">
         <button className=" text-left bg-colors-purple border-colors-purple text-colors-purpletext rotate-45 px-2 py-1 rounded-full">
-          N
+          Nursel
         </button>
 
         <button className="bg-transparent border-colors-transparent text-colors-graytext hover:text-colors-bluex  px-3 border  hover:border-colors-bluex  rounded-xl">
